48efde70f9cf02b8c4741e77939059d24a8fdff9
Tan Hoa <letanhoa2003@gmail.com>
2023-12-04 15:56:41 +0700

    update thanh trang

--- blog.js
+++ blog.js
@@ -5,48 +5,48 @@ let thisPage = 1;
 let limit = 6;
 let list = document.querySelectorAll('.list .article');
 
-function loadItem(){
+function loadItem() {
     let beginGet = limit * (thisPage - 1);
     let endGet = limit * thisPage - 1;
-    list.forEach((article, key)=>{
-        if(key >= beginGet && key <= endGet){
+    list.forEach((article, key) => {
+        if (key >= beginGet && key <= endGet) {
             article.style.display = 'block';
-        }else{
+        } else {
             article.style.display = 'none';
         }
     })
     listPage();
 }
 loadItem();
-function listPage(){
+function listPage() {
     let count = Math.ceil(list.length / limit);
     document.querySelector('.listPage').innerHTML = '';
 
-    if(thisPage != 1){
+    if (thisPage != 1) {
         let prev = document.createElement('li');
         prev.innerText = 'PREV';
         prev.setAttribute('onclick', "changePage(" + (thisPage - 1) + ")");
         document.querySelector('.listPage').appendChild(prev);
     }
 
-    for(i = 1; i <= count; i++){
+    for (i = 1; i <= count; i++) {
         let newPage = document.createElement('li');
         newPage.innerText = i;
-        if(i == thisPage){
+        if (i == thisPage) {
             newPage.classList.add('active');
         }
         newPage.setAttribute('onclick', "changePage(" + i + ")");
         document.querySelector('.listPage').appendChild(newPage);
     }
 
-    if(thisPage != count){
+    if (thisPage != count) {
         let next = document.createElement('li');
         next.innerText = 'NEXT';
         next.setAttribute('onclick', "changePage(" + (thisPage + 1) + ")");
         document.querySelector('.listPage').appendChild(next);
     }
 }
-function changePage(i){
+function changePage(i) {
     thisPage = i;
     loadItem();
 }
